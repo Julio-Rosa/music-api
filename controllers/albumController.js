33e013d2049ec824778bd5735f8db719cdf22321
Julio-Rosa <juliorosa.ibt@gmail.com>
2024-02-27 13:28:29 -0300

    album routes, album controller

---
+++ controllers/albumController.js
@@ -0,0 +1,106 @@
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const { Album } = require('../models/model');
+const crypto = require('crypto');
+
+async function insertAlbumData(name,image_url, artist_id) {
+    try {
+        const album = await Album.create({
+            album_id:crypto.randomUUID(),
+            name: name,
+            artist_id: artist_id,
+            image_url:image_url
+        });
+        return album;
+    } catch (error) {
+        console.error(`Error on create new album`, error);
+    }
+};
+
+async function findAllAlbums() {
+    try {
+        const albums = await Album.findAll();
+        if (albums.length > 0) {
+            return albums;
+        } else {
+            return 0;
+        }
+    } catch (error) {
+        console.error(`Error listing all albums`, error);
+    }
+
+};
+
+async function findAlbumById(album_id) {
+    try {
+        const album = await Album.findAll({
+            where: {
+                album_id: album_id
+            }
+        });
+
+        if (album.length > 0) {
+            return album;
+        }else{
+            return 0;
+        }
+    } catch (error) {
+        console.error(`Error when listing album with id ${album_id}`, error);
+    }
+}
+
+async function deleteAlbumById(album_id){
+     const album = await findAlbumById(album_id);
+     if(album == 0){
+        return 0;
+     }else{
+        try {
+            const deleted = await Album.destroy({
+                where: {
+                    album_id: album_id
+                }
+            });
+            if (deleted == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (error) {
+            console.error(`Error when deleting album with id "${album_id}"`, error);
+        }
+     }
+};
+
+async function updateAlbumById(album_id, name,image_url,artist_id){
+    const albumToUpdate = await findAlbumById(album_id);
+    if(albumToUpdate == 0){
+        return 0;
+    }else{
+        try {
+            const album = await Album.update({name:name,image_url:image_url,artist_id:artist_id},
+                {
+                    where:{
+                        album_id:album_id
+                        
+                    }
+                });
+                if(album[0] === 1){
+                    const updatedAlbum = await Album.findByPk(album_id);
+                    return updatedAlbum;
+                }else{
+                    return false;
+                }
+
+            
+        } catch (error) {
+            console.error(`Error when updating album with id "${album_id}"`, error);
+        }
+    }
+};
+
+module.exports = {
+    insertAlbumData,
+    findAllAlbums,
+    findAlbumById,
+    deleteAlbumById,
+    updateAlbumById
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const routes = express.Router();
+const { insertAlbumData, findAllAlbums, findAlbumById, deleteAlbumById, updateAlbumById } = require('../controllers/albumController');
 const { insertMusicData, findAllMusics, findAllMusicsByCategoryId, findAllMusicsByArtistId, findAllMusicsByAlbumId, deleteMusicBydId, updateMusicById, findById} = require('../controllers/musicController');
 const { Music } = require("../models/model");
 routes.use(express.json());
 
 
-
+//------------------- MUSIC ENDPOINTS--------------------------------------
 routes.post('/music/new', async (req, res) => {
     const { release_date, name, album_id, artist_id, category_id } = req.body;
     const music = await insertMusicData(release_date, name, album_id, artist_id, category_id);
@@ -96,6 +97,62 @@ routes.put('/music/update/:musicId', async (req, res) => {
     }
 });
 
+
+//----------------------------------ALBUM ENDPOINT------------------------------------------
+routes.post('/album/new', async (req, res) => {
+    const { name, image_url,artist_id } = req.body;
+    const album = await insertAlbumData(name,image_url, artist_id);
+    if (album) {
+        res.status(201).send(JSON.stringify(album));
+    } else {
+        res.status(500).send(JSON.stringify({ "message": "Error when creating a new album" }));
+    }
+
+});
+routes.get('/album/all', async (req, res) => {
+    const albums = await findAllAlbums();
+    if (albums == 0) {
+        res.status(404).send(JSON.stringify({ "message": "No albums found" }));
+    } else if (albums) {
+        res.status(200).send(JSON.stringify(albums));
+    } else {
+        res.status(500).send(JSON.stringify({ "message": "Error listing all albums" }));
+    }
+});
+routes.get('/album/:albumId', async (req, res) => {
+    const album = await findAlbumById(req.params.albumId);
+    if (album == 0) {
+        res.status(404).send(JSON.stringify({ "message": "Album not found!" }));
+    } else if (album) {
+        res.status(200).send(JSON.stringify(album));
+    } else {
+        res.status(500).send(JSON.stringify({ "message": "Error listing album by id" }));
+    }
+});
+routes.delete('/album/delete/:albumId', async (req, res) => {
+    const deleted = await deleteAlbumById(req.params.albumId);
+    if (deleted === 0) {
+
+        res.status(404).send(JSON.stringify({ "message": "Album Not Found" }));
+    } else {
+        if (deleted == true) {
+            res.status(200).send(JSON.stringify({ "message": "Deleted" }));
+        } else {
+            res.status(500).send(JSON.stringify({ "message": "An error occurred while deleting" }));
+        }
+    }
+});
+routes.put('/album/update/:albumId', async (req, res) => {
+    const { name, image_url,artist_id } = req.body;
+    const album = await updateAlbumById(req.params.albumId, name,image_url, artist_id);
+    if (album == 0 || album == false) {
+        res.status(404).send(JSON.stringify({ "message": "Album Not Found" }));
+    } else if (album) {
+        res.status(200).send(JSON.stringify(album));
+    } else {
+        res.status(500).send(JSON.stringify({ "message": "An error occurred while updating" }));
+    }
+});
 
 
 
